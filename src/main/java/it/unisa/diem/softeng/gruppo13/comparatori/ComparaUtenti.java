3abdfcc1c1e299bb23ed04712d6477102346c078
Andrea De Santis <Andrea De Santis@Andrea>
2025-12-03 16:10:28 +0100

    commenti

--- src/main/java/it/unisa/diem/softeng/gruppo13/comparatori/ComparaUtenti.java
+++ src/main/java/it/unisa/diem/softeng/gruppo13/comparatori/ComparaUtenti.java
@@ -9,14 +9,32 @@ import it.unisa.diem.softeng.gruppo13.elementi.Utente;
 import java.util.Comparator;
 
 /**
- *
- * @author Daniel
+ * @class ComparaUtenti
+ * @brief Implementa il comparatore per la classe `Utente`.
+ * 
+ * La classe `ComparaUtenti` implementa l'interfaccia `Comparator` per confrontare due oggetti `Utente`.
+ * Il metodo `compare` permette di stabilire l'ordine di confronto tra due utenti, che potrebbe essere basato 
+ * su diversi criteri, ad esempio il nome, il cognome, la matricola o l'email.
+ * Al momento, il metodo non è implementato.
+ * @author Daniel, Andrea, Stefano, Daniele
+ * @date Dicembre 03, 2025
  */
-public class ComparaUtenti implements Comparator<Utente>{
+public class ComparaUtenti implements Comparator<Utente> {
 
+    /**
+     * @brief Confronta due utenti per stabilirne l'ordine.
+     * 
+     * Questo metodo deve confrontare due oggetti `Utente` in base a un criterio specifico (ad esempio, nome, cognome, matricola, email).
+     * Attualmente, il metodo solleva un'eccezione `UnsupportedOperationException` in quanto non è ancora stato implementato.
+     * 
+     * @param o1 Primo utente da confrontare.
+     * @param o2 Secondo utente da confrontare.
+     * @return Un valore negativo se il primo utente è "minore" del secondo, zero se sono uguali, 
+     *         un valore positivo se il primo utente è "maggiore" del secondo, in base al criterio di confronto.
+     * @throws UnsupportedOperationException Se il metodo non è implementato.
+     */
     @Override
     public int compare(Utente o1, Utente o2) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose Tools | Templates.
     }
-    
 }
--- src/main/java/it/unisa/diem/softeng/gruppo13/logica/InterfacciaBiblioteca.java
+++ src/main/java/it/unisa/diem/softeng/gruppo13/logica/InterfacciaBiblioteca.java
@@ -12,26 +12,148 @@ import java.time.LocalDate;
 import java.util.List;
 
 /**
- *
- * @author Daniel
+ * @interface InterfacciaBiblioteca
+ * @brief Definisce le operazioni di gestione di una biblioteca.
+ * 
+ * L'interfaccia `InterfacciaBiblioteca` fornisce i metodi necessari per la gestione di una biblioteca, 
+ * incluse le operazioni per la gestione di libri, utenti e prestiti.
+ * Le classi che implementano questa interfaccia devono fornire le implementazioni per aggiungere, 
+ * cercare, rimuovere e restituire libri e utenti, nonché salvare i dati e gestire i prestiti.
+ * 
+ * @author Daniel, Andrea, Stefano, Daniele
+ * @date Dicembre 03, 2025
  */
-public interface InterfacciaBiblioteca {
-    
-    List<Libro> getLibri();
-    List<Utente> getUtenti();
-    List<Prestito> getPrestiti();
-    
-    void aggiungiLibro(String titolo, String autori, String anno, String isbn, String copie) throws Exception;
-    void aggiungiUtente(String nome, String cognome, String matricola, String email) throws Exception;
-    void aggiungiPrestito(Utente u, Libro l, LocalDate data) throws Exception;
-    
-    List<Libro> cercaLibri(String query);
-    List<Utente> cercaUtenti(String query);
 
-    void rimuoviLibro(Libro l);
-    void rimuoviUtente(Utente u);
-    void restituisciLibro(Prestito p);
+  
+  public interface InterfacciaBiblioteca {
     
-    void salvaTutto();
+    /**
+     * @brief Restituisce la lista di tutti i libri presenti nella biblioteca.
+     * 
+     * Questo metodo deve restituire una lista contenente tutti i libri registrati nella biblioteca.
+     * 
+     * @return Lista di oggetti `Libro` presenti nella biblioteca.
+     */
+       List<Libro> getLibri();
+
+     /**
+     * @brief Restituisce la lista di tutti gli utenti registrati nella biblioteca.
+     * 
+     * Questo metodo deve restituire una lista contenente tutti gli utenti registrati nella biblioteca.
+     * 
+     * @return Lista di oggetti `Utente` registrati nella biblioteca.
+     */
+       List<Utente> getUtenti();
+
+    /**
+     * @brief Restituisce la lista di tutti i prestiti attivi nella biblioteca.
+     * 
+     * Questo metodo deve restituire una lista contenente tutti i prestiti attivi nella biblioteca.
+     * 
+     * @return Lista di oggetti `Prestito` attivi nella biblioteca.
+     */
+       List<Prestito> getPrestiti();
+    
+    
+     /**
+     * @brief Aggiunge un nuovo libro alla biblioteca.
+     * 
+     * Questo metodo consente di aggiungere un nuovo libro alla biblioteca, specificando le informazioni 
+     * come il titolo, gli autori, l'anno di pubblicazione, il codice ISBN e il numero di copie disponibili.
+     * 
+     * @param titolo Titolo del libro da aggiungere.
+     * @param autori Autori del libro.
+     * @param anno Anno di pubblicazione del libro.
+     * @param isbn Codice ISBN del libro.
+     * @param copie Numero di copie disponibili del libro.
+     * @throws Exception Se i dati forniti non sono validi.
+     */
+      void aggiungiLibro(String titolo, String autori, String anno, String isbn, String copie) throws Exception;
+
+     /**
+     * @brief Aggiunge un nuovo utente alla biblioteca.
+     * 
+     * Questo metodo consente di aggiungere un nuovo utente alla biblioteca, fornendo i dati dell'utente 
+     * come nome, cognome, matricola e indirizzo email.
+     * 
+     * @param nome Nome dell'utente da aggiungere.
+     * @param cognome Cognome dell'utente da aggiungere.
+     * @param matricola Matricola identificativa dell'utente.
+     * @param email Indirizzo email dell'utente.
+     * @throws Exception Se i dati forniti non sono validi.
+     */
+      void aggiungiUtente(String nome, String cognome, String matricola, String email) throws Exception;
+
+    /**
+     * @brief Aggiunge un nuovo prestito.
+     * 
+     * Questo metodo consente di registrare un nuovo prestito, associando un utente a un libro con una data di restituzione.
+     * 
+     * @param u Utente che prende in prestito il libro.
+     * @param l Libro che viene preso in prestito.
+     * @param data Data di restituzione del libro.
+     * @throws Exception Se i dati forniti non sono validi.
+     */
+      void aggiungiPrestito(Utente u, Libro l, LocalDate data) throws Exception;
+    
+     
+    /**
+     * @brief Cerca i libri in base a una query di ricerca.
+     * 
+     * Questo metodo consente di cercare i libri che corrispondono alla query fornita e restituisce una lista di libri che soddisfano 
+     * i criteri di ricerca.
+     * 
+     * @param query Stringa di ricerca per trovare i libri.
+     * @return Lista di libri che corrispondono alla query di ricerca.
+     */
+      List<Libro> cercaLibri(String query);
+
+    /**
+     * @brief Cerca gli utenti in base a una query di ricerca.
+     * 
+     * Questo metodo consente di cercare gli utenti che corrispondono alla query fornita e restituisce una lista di utenti che soddisfano 
+     * i criteri di ricerca.
+     * 
+     * @param query Stringa di ricerca per trovare gli utenti.
+     * @return Lista di utenti che corrispondono alla query di ricerca.
+     */
+      List<Utente> cercaUtenti(String query);
+
+    
+     /**
+     * @brief Rimuove un libro dalla biblioteca.
+     * 
+     * Questo metodo consente di rimuovere un libro dalla biblioteca, eliminandolo dalla lista dei libri disponibili.
+     * 
+     * @param l Libro da rimuovere dalla biblioteca.
+     */
+     void rimuoviLibro(Libro l);
+
+    /**
+     * @brief Rimuove un utente dalla biblioteca.
+     * 
+     * Questo metodo consente di rimuovere un utente dalla biblioteca, eliminandolo dalla lista degli utenti registrati.
+     * 
+     * @param u Utente da rimuovere dalla biblioteca.
+     */
+     void rimuoviUtente(Utente u);
+
+    /**
+     * @brief Restituisce un libro preso in prestito.
+     * 
+     * Questo metodo consente di restituire un libro preso in prestito, aggiornando lo stato del prestito.
+     * 
+     * @param p Prestito da restituire.
+     */
+     void restituisciLibro(Prestito p);
+
+
+     /**
+     * @brief Salva tutti i dati della biblioteca.
+     * 
+     * Questo metodo consente di salvare tutte le informazioni riguardanti i libri, gli utenti e i prestiti 
+     * della biblioteca.
+     */
+     void salvaTutto();
   
 }
