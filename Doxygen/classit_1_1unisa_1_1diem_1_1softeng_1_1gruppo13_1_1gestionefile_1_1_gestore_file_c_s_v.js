edc50d2ad54aac4b4126bf677a2846d0394af8fd
dxnielb <bellodaniel04@gmail.com>
2025-12-06 15:54:00 +0100

    file doxygen

---
+++ Doxygen/classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo13_1_1gestionefile_1_1_gestore_file_c_s_v.js
@@ -0,0 +1,5 @@
+var classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo13_1_1gestionefile_1_1_gestore_file_c_s_v =
+[
+    [ "caricaDati", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo13_1_1gestionefile_1_1_gestore_file_c_s_v.html#a0c954532c3b74a375d1956e131c67c5d", null ],
+    [ "salvaFile", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo13_1_1gestionefile_1_1_gestore_file_c_s_v.html#a119488208211b77682f54eee59504505", null ]
+];
---
+++ Doxygen/classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo13_1_1interfacciagrafica_1_1_libro_controller.js
@@ -0,0 +1,4 @@
+var classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo13_1_1interfacciagrafica_1_1_libro_controller =
+[
+    [ "init", "classit_1_1unisa_1_1diem_1_1softeng_1_1gruppo13_1_1interfacciagrafica_1_1_libro_controller.html#abfcac4314c30d59a51ca4e937d2832e5", null ]
+];
